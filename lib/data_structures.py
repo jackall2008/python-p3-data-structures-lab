5ded698869b70022dd7e2f98eb36321edc9c6371
Julian Patterson <julian.patterson@gmail.com>
2023-03-01 10:22:37 -0500

    The data_structures.py file contains 7 functions written in Python code that pass 7 data structures tests.

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,3 +1,5 @@
+import ipdb
+
 spicy_foods = [
     {
         "name": "Green Curry",
@@ -17,22 +19,54 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    food_names = []
+    for food in spicy_foods:
+        if 'name' in food:
+            food_names.append(food['name'])
+    return food_names
+        
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = []
+
+    for food in spicy_foods:
+        if food['heat_level'] > 5:
+            spiciest_foods.append(food)
+    return spiciest_foods
+
 
 def print_spicy_foods(spicy_foods):
-    pass
+
+    for food in spicy_foods:
+        pepper_meter = food['heat_level'] * "🌶"
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {pepper_meter}")
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+
+    for food in spicy_foods:
+        if food['cuisine'] == cuisine:
+            return food
 
 def print_spiciest_foods(spicy_foods):
-    pass
+
+    for food in spicy_foods:
+        if food['heat_level'] > 5:
+            pepper_meter = food['heat_level'] * "🌶"
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: {pepper_meter}")
+
 
 def get_average_heat_level(spicy_foods):
-    pass
+    levels_of_heat = [food['heat_level'] for food in spicy_foods]
+
+    average = sum(levels_of_heat) / len(levels_of_heat)
+
+    return average
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+
+    added_spicy_foods = [food for food in spicy_foods]
+    
+    added_spicy_foods.append(spicy_food)
+
+    return added_spicy_foods
